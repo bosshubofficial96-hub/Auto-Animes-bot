ce342e9189dbda0133eeccdd2db6b0859d1af8bf
Bosshub <bosshubofficial96@gmail.com>
2026-05-18 22:49:31 +0530

    Delete bot directory

--- bot/services/_init_.py
+++
@@ -1,9 +0,0 @@
-"""
-Services module for ✦ ＡＵＴＯ ＡＮＩＭＥ ✦
-"""
-
-from bot.services.anilist_service import AniListService
-from bot.services.jikan_service import JikanService
-from bot.services.scheduler_service import SchedulerService
-
-__all__ = ['AniListService', 'JikanService', 'SchedulerService']
