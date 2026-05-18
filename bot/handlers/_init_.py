f56f1270eb7c40237aa0a531ca0ac84b968368ca
Bosshub <bosshubofficial96@gmail.com>
2026-05-18 13:07:35 +0530

    Create _init_.py

---
+++ bot/handlers/_init_.py
@@ -0,0 +1,10 @@
+"""
+Handlers module for ✦ ＡＵＴＯ ＡＮＩＭＥ ✦
+"""
+
+from bot.handlers.admin_handlers import AdminHandlers
+from bot.handlers.user_handlers import UserHandlers
+from bot.handlers.callback_handlers import CallbackHandlers
+from bot.handlers.error_handlers import ErrorHandlers
+
+__all__ = ['AdminHandlers', 'UserHandlers', 'CallbackHandlers', 'ErrorHandlers']
